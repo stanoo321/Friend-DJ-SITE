f38fb6fdca227b8eb979526906483ff9a0a420ae
stanoo321 <75757730+stanoo321@users.noreply.github.com>
2021-06-10 15:44:11 -0700

    add dj site

---
+++ javascript/script.js
@@ -0,0 +1,59 @@
+$(function(){
+    $("#signUpButton").click(function(){
+        $("#signUpModal").modal("show");
+    });
+});
+
+$(function(){
+    $(".close").click(function(){
+        $("#signUpModal").modal("hide");
+    });
+});
+
+
+$(function(){
+    $("#rsvpBtn").click(function(){
+        $("#facebookModal").modal("show");
+    });
+});
+
+$(function(){
+    $("#rsvpBtn2").click(function(){
+        $("#facebookModal").modal("show");
+    });
+});
+
+$(function(){
+    $("#rsvpBtn3").click(function(){
+        $("#facebookModal").modal("show");
+    });
+});
+
+$(function(){
+    $(".close").click(function(){
+        $("#signUpModal").modal("hide");
+    });
+});
+
+$(function() {
+    $("#shopHatButton").click(function() {
+        $("#shopHatModal").modal("toggle");
+    });
+});
+
+$(function() {
+    $("#shopHoodieButton").click(function() {
+        $("#shopHoodieModal").modal("toggle");
+    });
+});
+
+$(function() {
+    $("#shopShirtButton").click(function() {
+        $("#shopShirtModal").modal("toggle");
+    });
+});
+$(function() {
+    $("#cartButton").click(function() {
+        $("#cartModal").modal("toggle");
+    });
+});
